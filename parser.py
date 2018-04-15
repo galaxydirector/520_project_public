8562388f7cb1473114f1f0a87b88f38b50a2480e
Haoxian Chen <hxchen414@gmail.com>
2018-04-15 10:32:21 -0400

    [parser] Tweak.

--- parser.py
+++ parser.py
@@ -5,10 +5,7 @@ import os
 nltk.data.path.append(os.getcwd() + '/dataset/')
 from nltk.corpus import semcor
 
-from nltk.stem.wordnet import WordNetLemmatizer
-import re
 import pickle
-import numpy as np
 
 def load_tag_fies(index_file):
     tag_files = []
@@ -30,22 +27,6 @@ def save(data, filename):
     with open(filename, 'w') as f:
         pickle.dump([data,], f)
 
-class WordParser:
-    def __init__(self):
-        self.pattern = re.compile('[a-z]+')
-
-    def filter(self,line):
-        return ('WORD: ' in line)
-
-    def map(self,word):
-        return self.pattern.findall(word)[0]
-
-class WordFilter:
-    def __init__(self, min_sense_appre=10):
-        self.min_sense_appre = min_sense_appre
-    def filter(self, stat):
-        return (len(stat.values()) >= self.min_sense_appre)
-
 class WordStat:
     def __init__(self, init_map={}):
         self.senses = init_map
@@ -64,10 +45,10 @@ class WordStat:
     def items(self):
         return self.senses.items()
 
-class SemcorParser:
+class CorpusParser:
     def __init__(self, index_file, force_update=False):
         # try to load from pickle file first
-        pkl_file = 'pickle/word_map.pkl'
+        pkl_file = 'pickle/%s_word_map.pkl' % os.path.basename(index_file).split('.')[0]
         loaded = load(pkl_file)
 
         if loaded != None and force_update==False:
@@ -94,25 +75,6 @@ class SemcorParser:
                     self.word_map[lemma] = WordStat()
                 self.word_map[lemma].update(sense_id)
 
-class WordContext:
-    class Context:
-        def __init__(self,sent,sense_id):
-            self.sent = sent
-            self.sense_id = sense_id
-        def __str__(self):
-            _str = ''
-            for wf in self.sent.getchildren():
-                _str += '%s ' % wf.text
-            return _str
-
-    def __init__(self):
-        self.stats = []
-    def update(self, sent, sense_id):
-        self.stats.append(Context(sent, sense_id))
-    def dump(self):
-        for i,c in enumerate(self.stats):
-            print 'context%d: %s' % (i,str(c))
-
 def filter_word_map(word_map, min_sense_appr):
     filtered_map = {}
     for word, senses in word_map.items():
@@ -128,7 +90,7 @@ def filter_word_map(word_map, min_sense_appr):
 
 if __name__ == '__main__':
     tag_file = './dataset/semcor_tagfiles_full.txt'
-    word_map = SemcorParser(tag_file,force_update=False).word_map
+    word_map = CorpusParser(tag_file,force_update=False).word_map
 
     MIN_SENSE_APPR = 1000
     ambiguous_words = filter_word_map(word_map, MIN_SENSE_APPR)
