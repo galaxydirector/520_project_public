31c5466656403886440547069cf9acb816450ace
Haoxian Chen <hxchen414@gmail.com>
2018-04-17 14:41:07 -0400

    [parse] Train Word2Vec Model using local semcor corpus.

--- parser.py
+++ parser.py
@@ -10,6 +10,34 @@ from gensim.models import Word2Vec
 from extractor import *
 from misc import *
 
+def scan_corpus(index_file, f_parse):
+    tag_files = load_tag_fies(index_file)
+    for filename in tag_files:
+        print 'parsing %s...' % filename
+        sentences = semcor.xml(filename).findall('context/p/s')
+        f_parse(sentences)
+
+class ModelTrainer:
+    def __init__(self):
+        self._model = Word2Vec(size=100, window=5, min_count=5, workers=2)
+        self._trained = False
+    def parse(self, sentences):
+        sent_str = map(lambda x:self.__semcor_sent2str(x), sentences)
+        if not self._trained:
+            self.model.build_vocab(sent_str)
+            self._trained = True
+        else:
+            self.model.build_vocab(sent_str,update=True)
+        self.model.train(sent_str,total_examples=self.model.corpus_count,epochs=self.model.iter)
+
+    @staticmethod
+    def __semcor_sent2str(sent):
+        return map(lambda x: x.text, sent);
+
+    @property
+    def model(self):
+        return self._model
+
 class WordStat:
     def __init__(self, init_map={}):
         self.senses = init_map
@@ -31,7 +59,9 @@ class WordStat:
 class CorpusParser:
     def __init__(self, index_file, force_update=False):
         # try to load from pickle file first
-        pkl_file = 'pickle/%s_word_map.pkl' % os.path.basename(index_file).split('.')[0]
+        pkl_dir = './pickle'
+        pkl_file = '%s/%s_word_map.pkl' % (pkl_dir,
+                os.path.basename(index_file).split('.')[0])
         loaded = load(pkl_file)
 
         if loaded != None and force_update==False:
@@ -42,6 +72,10 @@ class CorpusParser:
             tag_files = load_tag_fies(index_file)
             for f in tag_files:
                 self.parse(f)
+
+            if not os.path.exists(pkl_dir):
+                os.makedirs(pkl_dir)
+
             save(self.word_map, pkl_file)
 
     def parse(self,filename):
@@ -71,29 +105,35 @@ def filter_word_map(word_map, min_sense_appr):
             print sid, count
     return filtered_map
 
-def load_word2vec_model():
-    model_file = 'semcor.embedding'
+def load_word2vec_model(index_file, force_update=False):
+    model_dir = 'model'
+    model_file = './%s/%s' % (model_dir,'semcor.embedding')
 
-    if os.path.isfile(model_file):
+    if os.path.isfile(model_file) and not force_update:
         print 'Load from %s' % model_file
         model = Word2Vec.load(model_file)
     else:
-        model = Word2Vec(semcor.sents(), size=100, window=5, min_count=5, workers=2)
+        trainer = ModelTrainer()
+        scan_corpus(index_file, trainer.parse)
+        model = trainer.model
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
         model.save(model_file)
         print 'Saved model to %s' % model_file
     return model
 
 if __name__ == '__main__':
-    word2vec_model = load_word2vec_model()
+    # index_file = './dataset/semcor_tagfiles_full.txt'
+    index_file = './dataset/brown1_tagfiles.txt'
+    
+    word2vec_model = load_word2vec_model(index_file, force_update=True)
 
-    tag_file = './dataset/semcor_tagfiles_full.txt'
-    # tag_file = './dataset/brown1_tagfiles.txt'
-    word_map = CorpusParser(tag_file,force_update=False).word_map
+    word_map = CorpusParser(index_file,force_update=False).word_map
 
     MIN_SENSE_APPR = 1000
     ambiguous_words = filter_word_map(word_map, MIN_SENSE_APPR)
     # print '%d ambiguous words' % len(ambiguous_words.keys())
 
     ce = ContextExtractor(word_map, word2vec_model)
-    ce.go(tag_file)
+    ce.go(index_file)
     ce.dump2file()
